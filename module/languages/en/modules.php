98f04583bf2692b84e8d7e00dc220b2a9bc82606
Tristan Lins <tristan.lins@bit3.de>
2014-03-11 15:54:25 +0100

    Extract the subscription system into separate project.

---
+++ module/languages/en/modules.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-subscription
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Module
+ */
+$GLOBALS['TL_LANG']['MOD']['avisota-subscription'] = array(
+	'Avisota - Subscription',
+	'Subscription system for Avisota.'
+);
---
+++ module/languages/en/orm_avisota_message.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-message
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['orm_avisota_message']['setRecipients'] = array(
+	'Select recipients',
+	'Select the recipients for this newsletter.'
+);
+$GLOBALS['TL_LANG']['orm_avisota_message']['recipients']    = array(
+	'Recipients',
+	'Please chose the recipients for this newsletter.'
+);
