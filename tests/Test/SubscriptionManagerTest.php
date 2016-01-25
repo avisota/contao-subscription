df794d24e1e8e1df8f3a59c86e70a4167317875c
Sven Baumann <baumann.sv@gmail.com>
2016-01-25 10:12:05 +0100

    make travis happy add clean phpunit test

---
+++ tests/Test/SubscriptionManagerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright © 2016 Sven Baumann
+ *
+ * PHP version 5
+ *
+ * @copyright  way.vision 2016
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @package    avisota/contao-core
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+namespace Avisota\Test\Contao\Subscription;
+
+/**
+ * Class SubscriptionManagerTest
+ *
+ * @package Avisota\Test\Contao\Subscription
+ */
+class SubscriptionManagerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsBlacklisted()
+    {
+    }
+}
